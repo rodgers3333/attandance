cdca3060f36b035bc6e55db84c65df9b1a69a7fd
rodgers3333 <brandonrodgers3333@gmail.com>
2025-02-02 03:14:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyRzBnlL4MzEuQkRgagie_XvoDmuGUFHBpLicRSFbA9H0xvxrIDzMmp4K-yoFG4lNjE/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyRzBnlL4MzEuQkRgagie_XvoDmuGUFHBpLicRSFbA9H0xvxrIDzMmp4K-yoFG4lNjE/exec';
 const form = document.forms['submit-to-google-sheet'];
 const msg = document.getElementById("msg");
 
 form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then(response => {
-        msg.innerHTML = "Connection failed! Try again Later";
-        msg.style.color = "black";
-        setTimeout(function(){
-            msg.innerHTML = ""
-        },5000)
-        form.reset()
+        if (response.ok) {
+            msg.innerHTML = "Form submitted successfully!";
+            msg.style.color = "green";
+            setTimeout(function() {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        } else {
+            throw new Error('Connection failed');
+        }
     })
-    .catch(error => console.error('Error!', error.message))
-})
+    .catch(error => {
+        msg.innerHTML = "Connection failed! Try again later";
+        msg.style.color = "red";
+        setTimeout(function() {
+            msg.innerHTML = "";
+        }, 5000);
+        console.error('Error!', error.message);
+    });
+});
