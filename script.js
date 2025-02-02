2fc2cb37c972003995adf1fe978cf034f94f4b99
rodgers3333 <brandonrodgers3333@gmail.com>
2025-02-02 02:52:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyRzBnlL4MzEuQkRgagie_XvoDmuGUFHBpLicRSFbA9H0xvxrIDzMmp4K-yoFG4lNjE/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Connection failed! Try again Later";
+        msg.style.color = "black";
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
